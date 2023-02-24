25e7fa5735abfd425d969e2d38ba57a71c506ebc
SyedAliRaza04 <SyedAliRaza04@users.noreply.github.com>
2023-02-24 14:09:38 +0000

    Apply Black formatting

--- extract_controlnet.py
+++ extract_controlnet.py
@@ -4,8 +4,16 @@ from safetensors.torch import load_file, save_file
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--src", default=None, type=str, required=True, help="Path to the model to convert.")
-    parser.add_argument("--dst", default=None, type=str, required=True, help="Path to the output model.")
+    parser.add_argument(
+        "--src",
+        default=None,
+        type=str,
+        required=True,
+        help="Path to the model to convert.",
+    )
+    parser.add_argument(
+        "--dst", default=None, type=str, required=True, help="Path to the output model."
+    )
     args = parser.parse_args()
 
     assert args.src is not None, "Must provide a model path!"
@@ -15,10 +23,14 @@ if __name__ == "__main__":
         state_dict = load_file(args.src)
     else:
         state_dict = torch.load(args.src)
-        
+
     if any([k.startswith("control_model.") for k, v in state_dict.items()]):
-        state_dict = {k.replace("control_model.", ""): v for k, v in state_dict.items() if k.startswith("control_model.")}
-    
+        state_dict = {
+            k.replace("control_model.", ""): v
+            for k, v in state_dict.items()
+            if k.startswith("control_model.")
+        }
+
     if args.dst.endswith(".safetensors"):
         save_file(state_dict, args.dst)
     else:
--- preload.py
+++ preload.py
@@ -1,3 +1,13 @@
 def preload(parser):
-    parser.add_argument("--controlnet-dir", type=str, help="Path to directory with ControlNet models", default=None)
-    parser.add_argument("--no-half-controlnet", action='store_true', help="do not switch the ControlNet models to 16-bit floats (only needed without --no-half)", default=None)
+    parser.add_argument(
+        "--controlnet-dir",
+        type=str,
+        help="Path to directory with ControlNet models",
+        default=None,
+    )
+    parser.add_argument(
+        "--no-half-controlnet",
+        action="store_true",
+        help="do not switch the ControlNet models to 16-bit floats (only needed without --no-half)",
+        default=None,
+    )
